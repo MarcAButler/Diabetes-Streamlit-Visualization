399d43c0fbd6b0ac9d16e75a22294c24a7b505a6
MarcAButler <marc.a.butler.1999@gmail.com>
2023-03-01 01:51:09 -0500

    Got slider working for area chart

--- app.py
+++ app.py
@@ -19,7 +19,34 @@ st.set_page_config(layout="wide")
 # DATA #
 ########
 
+ageGroupMapping = {
+    1: "18-24",
+    2: "25-29",
+    3: "30-34",
+    4: "35-39",
+    5: "40-44",
+    6: "45-49",
+    7: "50-54",
+    8: "55-59",
+    9: "60-64",
+    10: "65-69", 
+    11: "70-74", 
+    12: "75-79", 
+    13: "> 80" 
+}
+
 diabetes_binary_df = pd.read_csv("diabetes_binary_split_health_indicators.csv")
+
+# Loop through sex to change binary to strings
+# for index, value in enumerate(diabetes_binary_df["Sex"]):
+#     diabetes_binary_df["Sex"][index] = "male" if diabetes_binary_df["Sex"][index] == 1 else "female"
+diabetes_binary_df["Sex"].replace({0.0: "♀️ Female", 1.0: "♂️ Male"}, inplace=True)
+
+
+# Loop through age to change binary to strings
+diabetes_binary_df["Age"].replace(ageGroupMapping, inplace=True)
+print(diabetes_binary_df["Age"])
+
 diabetes_binary_df.head()
 
 diabetes_df = pd.read_csv("diabetes_mortality_by_state.csv")
@@ -68,21 +95,39 @@ if st.sidebar.checkbox("🧠Mental Health"):
     "Mental Health should be toggled"
 
 
-ageRanges = {
-    "18-24": 1,
-    "25-29": 2,
-    "30-34": 3,
-    "35-39": 4,
-    "40-44": 5,
-    "45-49": 6,
-    "50-54": 7,
-    "55-59": 8,
-    "60-64": 9,
-    "65-69": 10,
-    "70-74": 11,
-    "75-79": 12,
-    "> 80": 13,
-}
+
+# ageRanges = {
+#     "18-24": 1,
+#     "25-29": 2,
+#     "30-34": 3,
+#     "35-39": 4,
+#     "40-44": 5,
+#     "45-49": 6,
+#     "50-54": 7,
+#     "55-59": 8,
+#     "60-64": 9,
+#     "65-69": 10,
+#     "70-74": 11,
+#     "75-79": 12,
+#     "> 80": 13,
+# }
+
+ageRanges = [
+    "18-24",
+    "25-29",
+    "30-34",
+    "35-39",
+    "40-44",
+    "45-49",
+    "50-54",
+    "55-59",
+    "60-64",
+    "65-69",
+    "70-74",
+    "75-79",
+    "> 80"
+]
+
 minAgeGroup, maxAgeGroup = st.sidebar.select_slider(
     "Age Groups",
     # 18, 80,
@@ -90,6 +135,27 @@ minAgeGroup, maxAgeGroup = st.sidebar.select_slider(
     value=["18-24", "> 80"],
     options=ageRanges
 )
+
+minIndex = ageRanges.index(minAgeGroup)
+maxIndex = ageRanges.index(maxAgeGroup)
+
+ageRangesSelected = ageRanges[minIndex:]
+ageRangesSelected = ageRangesSelected[:maxIndex]
+print("ageRangesSelected: ", ageRangesSelected)
+
+print("minAgeGroup: ", minAgeGroup, "maxAgeGroup: ", maxAgeGroup)
+# def remove_outside_of_min_and_max_age_groups(rate):
+#     if rate >= minAvgDeathRate and rate <= maxAvgDeathRate:
+#         return True
+#     else:
+#         return False
+
+# diabetes_binary_df['Age'] = list(filter(remove_outside_of_min_and_max_age_groups, diabetes_binary_df['Age']))
+
+diabetes_binary_df = diabetes_binary_df[diabetes_binary_df["Age"].isin(ageRangesSelected)]
+
+#  Convert to DataFrame
+# diabetes_binary_df['Age'] = pd.DataFrame(filtered_age_groups, columns = ['RATE'])
 
 
 # if st.sidebar.checkbox("18-24"):
@@ -177,38 +243,49 @@ with container:
         #     # [diabetes_binary_df["Diabetes_binary"].values.toList(), diabetes_binary_df["Age"].values.toList()],
         #     columns=['♂️', '♀️'])
         
-        chart_data = pd.DataFrame(
-            np
-        )
 
-        chart_data = diabetes_binary_df[["Diabetes_binary", "Age"]]
-        # chart_data = diabetes_binary_df[["Diabetes_binary", "Age", "Sex"]]
-       
-        # st.write(chart_data.head())
+        chart_data = diabetes_binary_df[["Diabetes_binary", "Age", "Sex"]]
         
-        chart_data = chart_data.groupby('Age', as_index=False).sum()
-        # chart_data = chart_data.groupby(["Age", "Sex"], as_index=False).sum()
+        chart_data = chart_data.groupby(["Age", "Sex"], as_index=False).sum()
 
-        chart_data.rename(columns={"A": "a", "B": "c"})
-
-        st.write(chart_data.groupby("Age", as_index=False).sum().head())
-        # st.write(chart_data.groupby(["Age", "Sex"], as_index=False).sum().head())
-
-
-        # Add Sex to chart_data
-        # chart_data["Sex"] = ['♂️', '♀️']
-        # chart_data.columns=['♂️', '♀️']
-
-        # Use go from plotly.graph._objs as go function for plan b
-        st.area_chart(
-            # diabetes_binary_df[["Age", "Diabetes_binary"]],
-            # [diabetes_binary_df["Diabetes_binary"].sum(), diabetes_binary_df["Age"]],
-            chart_data,
-            # chart_data.groupby('Age').sum(),
-            height=200,
-            x="Age",
-            y="Diabetes_binary"
+        area_chart = alt.Chart(chart_data).mark_area().encode(
+            x=alt.X("Age:O", axis=alt.Axis(labelAngle=-45)),
+            y=alt.Y("Diabetes_binary:Q", title="Population with Diabetes"),
+            color=alt.Color("Sex:O", scale=alt.Scale(range=["#ffc8dd", "#bde0fe"]))
         )
+
+        
+
+        st.altair_chart(area_chart, use_container_width=True)
+
+        # chart_data = diabetes_binary_df[["Diabetes_binary", "Age"]]
+        # # chart_data = diabetes_binary_df[["Diabetes_binary", "Age", "Sex"]]
+       
+        # # st.write(chart_data.head())
+        
+        # chart_data = chart_data.groupby('Age', as_index=False).sum()
+        # # chart_data = chart_data.groupby(["Age", "Sex"], as_index=False).sum()
+
+        # chart_data.rename(columns={"A": "a", "B": "c"})
+
+        # st.write(chart_data.groupby("Age", as_index=False).sum().head())
+        # # st.write(chart_data.groupby(["Age", "Sex"], as_index=False).sum().head())
+
+
+        # # Add Sex to chart_data
+        # # chart_data["Sex"] = ['♂️', '♀️']
+        # # chart_data.columns=['♂️', '♀️']
+
+        # # Use go from plotly.graph._objs as go function for plan b
+        # st.area_chart(
+        #     # diabetes_binary_df[["Age", "Diabetes_binary"]],
+        #     # [diabetes_binary_df["Diabetes_binary"].sum(), diabetes_binary_df["Age"]],
+        #     chart_data,
+        #     # chart_data.groupby('Age').sum(),
+        #     height=200,
+        #     x="Age",
+        #     y="Diabetes_binary"
+        # )
 
     with col2:
         
